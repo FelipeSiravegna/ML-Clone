33f6e49d2df891288d4fb0c5d2f399ecda7443ce
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-06-17 23:12:30 -0300

    Bottom row and details from top row

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,9 +15,26 @@ export default function NavBar(){
                 </div>
             </div>
             <div className={style.bottomRow}>
-                <div>Dirección</div>
-                <div>Menú</div>
-                <div>Cuenta</div>
+                <div className={style.direccion}>
+                    <p className={style.enviarA}>Enviar a</p>
+                    <p className={style.ciudad}>Capital Federal</p>
+                </div>
+                <div className={style.menuContainer}>
+                    <p className={style.menuItem}>Categorías</p>
+                    <p className={style.menuItem}>Ofertas</p>
+                    <p className={style.menuItem}>Historial</p>
+                    <p className={style.menuItem}>Supermercado</p>
+                    <p className={style.menuItem}>Moda</p>
+                    <p className={style.menuItem}>Mercado Play</p>
+                    <p className={style.menuItem}>Vender</p>
+                    <p className={style.menuItem}>Ayuda</p>
+                </div>
+                <div className={style.cuentaContainer}>
+                    <p>Creá tu cuenta</p>
+                    <p>Ingresá</p>
+                    <p>Mis compras</p>
+                    <img className={style.carritoImagen} src="src/assets/shopping-cart.png"/>
+                </div>
             </div>
         </div>
     )
